1f49f7dc692492c20035c317837876a8cccb8ad9
bruno villasenor <bvillasen@gmail.com>
2021-06-28 13:12:04 -0700

    fixed

--- tools/reduce_cholla_snapshot_files.py
+++ tools/reduce_cholla_snapshot_files.py
@@ -96,7 +96,7 @@ for snapshot_id in snapshots_to_copy:
   if rank == 0: 
     files_copied = os.listdir( dst_dir )  
     if len( files_copied ) != n_snaps_copied * files_per_snapshot: 
-      print(f'ERROR: Number of files in output dir is incorrect: {files_copied}    {n_snaps_copied * files_per_snapshot}')
+      print(f'ERROR: Number of files in output dir is incorrect: {len(files_copied)}    {n_snaps_copied * files_per_snapshot}')
       exit(-1)
   
   if rank == 0: print_progress( n_snaps_copied, n_snapshots*n_sims, time_start )
