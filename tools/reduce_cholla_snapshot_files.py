4cb18ae2208f1bba546669fc635a58b061ccc57c
bruno villasenor <bvillasen@gmail.com>
2021-06-28 13:09:26 -0700

    fixed

--- tools/reduce_cholla_snapshot_files.py
+++ tools/reduce_cholla_snapshot_files.py
@@ -34,7 +34,7 @@ if data_type == 'particles': file_name_base = '_particles.h5'
 
 data_dir = '/gpfs/alpine/csc434/proj-shared/cholla/'
 root_dir = data_dir + 'cosmo_sims/sim_grid/1024_P19m_np4_nsim256/'
-output_dir = data_dir + 'cosmo_sims/sim_grid/1024_P19m_np4_nsim256/reduced_snapshot_files/'
+output_dir = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim256/reduced_snapshot_files_{data_type}/'
 if rank == 0: create_directory( output_dir )
 
 
