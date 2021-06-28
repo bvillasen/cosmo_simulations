71c0c21adf636b332817026279e65aef260b36c3
bruno villasenor <bvillasen@gmail.com>
2021-06-28 12:37:56 -0700

    mnew

--- tools/reduce_cholla_snapshot_files.py
+++ tools/reduce_cholla_snapshot_files.py
@@ -20,6 +20,9 @@ print_out = True if rank == 0 else False
 data_type = 'hydro'
 # data_type = 'particles'
 
+if data_type == 'hydro': file_name_base = '.h5'
+if data_type == 'hydro': file_name_base = '_particles.h5' 
+
 data_dir = '/gpfs/alpine/csc434/proj-shared/cholla/'
 root_dir = data_dir + 'cosmo_sims/sim_grid/1024_P19m_np4_nsim256/'
 output_dir = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/reduced_snapshot_files/'
@@ -28,16 +31,34 @@ if rank == 0: create_directory( output_dir )
 
 
 input_dir = root_dir + f'snapshot_files_{data_type}/'
-snapshot_dirs = os.listdir( input_dir ) 
-snapshot_dirs.sort()
+simulations_dirs = os.listdir( input_dir ) 
+simulations_dirs.sort()
 
 snapshot_ids = range( 1, 17 )
 files_per_snapshot = 128
+local_files = split_indices( range(files_per_snapshot), rank, n_procs )
 
 
-snapshot_dir = snapshot_dirs[0]
-if print_out: print( f'Copying {snapshot_dir}' ) 
+
+simulation_dir = input_dir + simulations_dirs[0] + '/'
+dst_dir = output_dir + simulations_dirs[0] + '/'
+if print_out: 
+  print( f'Copying: {simulation_dir}' ) 
+  print( f'Destiny: {dst_dir}' )
+
 snapshot_id = snapshot_ids[0]
 if print_out: print( f' Copying snapshot: {snapshot_id}' )
+
+for file_id in local_files:
+
+  file_name = f'{snapshot_id}{file_name_base}.{file_id}'
+  print( file_name )
+  # in_file = h5
+  
+  
+
+
+
+
 
 
