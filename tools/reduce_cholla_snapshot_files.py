86b90a97bcf79c3f01b10572b0c20255e165f6b0
bruno villasenor <bvillasen@gmail.com>
2021-06-28 12:19:46 -0700

    updated

--- tools/reduce_cholla_snapshot_files.py
+++ tools/reduce_cholla_snapshot_files.py
@@ -14,6 +14,9 @@ else:
   rank = 0
   n_procs = 1
 
+# print_out = False
+print_out = True if rank == 0 else False
+
 data_type = 'hydro'
 # data_type = 'particles'
 
@@ -22,7 +25,19 @@ root_dir = data_dir + 'cosmo_sims/sim_grid/1024_P19m_np4_nsim256/'
 output_dir = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/reduced_snapshot_files/'
 if rank == 0: create_directory( output_dir )
 
+
+
 input_dir = root_dir + f'snapshot_files_{data_type}/'
 snapshot_dirs = os.listdir( input_dir ) 
+snapshot_dirs.sort()
+
+snapshot_ids = range( 1, 17 )
+files_per_snapshot = 128
+
+
+snapshot_dir = snapshot_dirs[0]
+if rank == 0: 
+snapshot_id = snapshot_ids[0]
+
 
 
