8f12d156c6984dc228814510334f2f480179e545
bruno villasenor <bvillasen@gmail.com>
2021-06-30 16:25:29 -0700

    updated

--- tools/make_snapshot_slices.py
+++ tools/make_snapshot_slices.py
@@ -14,7 +14,7 @@ sys.path.extend(subDirectories)
 from load_data import load_snapshot_data_distributed
 from tools import *
 
-use_mpi = True
+use_mpi = False
 if use_mpi :
   from mpi4py import MPI
   comm = MPI.COMM_WORLD
@@ -30,45 +30,44 @@ if rank == 0: show_progess = True
 
 
 # data_dir = '/raid/bruno/data/'
-# data_dir = '/data/groups/comp-astro/bruno/'
-data_dir = '/gpfs/alpine/csc434/proj-shared/cholla/'
-input_dir  = data_dir + f'cosmo_sims/rescaled_P19/1024_50Mpc/snapshot_files/'
-output_dir = data_dir + f'cosmo_sims/rescaled_P19/1024_50Mpc/slices/'
+data_dir = '/data/groups/comp-astro/bruno/'
+# data_dir = '/gpfs/alpine/csc434/proj-shared/cholla/'
+input_dir  = data_dir + f'cosmo_sims/rescaled_P19/2048_50Mpc/reduced_snapshot_hydro/'
+output_dir = data_dir + f'cosmo_sims/rescaled_P19/2048_50Mpc/slices/'
 if rank == 0: create_directory( output_dir )
   
-n_points = 1024
+n_points = 2048
 Lbox = 50000.0 #kpc/h
 box_size = [ Lbox, Lbox, Lbox ]
 grid_size = [ n_points, n_points, n_points ]
 precision = np.float32
   
-fields = [ 'density' ]
-data_types = [ 'hydro', 'particles' ]
+fields = [ 'temperature' ]
+data_type 'hydro'
 
-for data_type in data_types:
+slice_depth = 128
+slice_id = 3
+slice_start = slice_id * slice_depth
+start = max( 0, slice_start )
+end   = min( n_points, slice_start+slice_depth )
+subgrid = [ [start, end], [0, n_points], [0, n_points] ]
 
-  n_snap = rank+1 
-  data_snap = load_snapshot_data_distributed( data_type, fields, n_snap, input_dir, box_size, grid_size,  precision, show_progess=show_progess )
-  current_z = data_snap['Current_z']
 
-  slice_depth = 64
-  n_slices = n_points // slice_depth 
-  slice_id = 3
-  slice_start = slice_id * slice_depth
+n_snap = 169 
+data_snap = load_snapshot_data_distributed( data_type, fields, n_snap, input_dir, box_size, grid_size,  precision, subgrid=subgrid, show_progess=show_progess )
+current_z = data_snap['Current_z']
 
-  start = max( 0, slice_start )
-  end   = min( n_points, slice_start+slice_depth )
-  # print( f' Slice:  start:{start}   end:{end}' )
+# print( f' Slice:  start:{start}   end:{end}' )
 
-  out_file_name = output_dir + f'slice_{data_type}_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
-  outfile = h5.File( out_file_name, 'w' )
-  outfile.attrs['current_z'] = current_z
+out_file_name = output_dir + f'slice_{data_type}_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
+outfile = h5.File( out_file_name, 'w' )
+outfile.attrs['current_z'] = current_z
 
-  for field in fields:
-    data = data_snap[field]
-    data_slice = data[slice_start:end, :, :] 
-    outfile.create_dataset( field, data=data_slice )
-
-  outfile.close()
-  print( f'Saved File: {out_file_name}' )
+# for field in fields:
+#   data = data_snap[field]
+#   data_slice = data[slice_start:end, :, :] 
+#   outfile.create_dataset( field, data=data_slice )
+# 
+# outfile.close()
+# print( f'Saved File: {out_file_name}' )
 
