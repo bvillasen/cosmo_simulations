16deaef31cc8a1e23d3cd1856dbfb772b5e86d84
bruno <bvillasen@gmail.com>
2021-07-01 06:33:46 -0700

    fixed

--- tools/select_files_by_redshift.py
+++ tools/select_files_by_redshift.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import h5py as h5
 from tools import *
+from shutil import copyfile
 
 data_dir = '/data/groups/comp-astro/bruno/'
 input_dir  = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/slices_temperature/'
@@ -26,3 +27,20 @@ for z in selected_z:
   diff = np.abs( z_vals - z )
   indx = np.where( diff == diff.min() )[0][0]
   selected_snaps.append( indx )
+
+for snap_id in selected_snaps:
+  src_file = input_dir  + f'slice_hydro_{snap_id}_start{slice_start}_depth{slice_depth}.h5'
+  dst_file = output_dir + f'slice_hydro_{snap_id}_start{slice_start}_depth{slice_depth}.h5'
+  copyfile( src_file, dst_file )
+  
+
+
+
+
+
+
+
+
+
+
+
