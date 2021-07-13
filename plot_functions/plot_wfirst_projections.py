26face30e4751ac5dac19b8fa70b0533f03cbaab
bruno <bvillasen@gmail.com>
2021-07-13 13:07:34 -0700

    new

--- plot_functions/plot_wfirst_projections.py
+++ plot_functions/plot_wfirst_projections.py
@@ -15,31 +15,40 @@ from tools import *
 from colors import * 
 
 
-input_dir = data_dir + 'cosmo_sims/wfirst_1024/snapshots/h5_files/'
+input_dir = data_dir + 'cosmo_sims/wfirst_1024/snapshots/h5_files/grid_files/'
 output_dir = data_dir + 'cosmo_sims/wfirst_1024/figures/'
 create_directory( output_dir ) 
 
 
-slice_start = 0
 slice_width = 64
+slice_start = 14*slice_width
+slice_start = 0*slice_width
 
-
+dens_max = 2e6 /100
+dens_min = 1e1
 projections = {}
 
-n_snap = 500
-i = 0
-file_name = input_dir + f'grid_CIC_{n_snap:03}.h5'
-file = h5.File( file_name, 'r' )
-current_z = file.attrs['current_z'][0]
-density = file['density']
-slice = density[slice_start:slice_start+slice_width, :, :]
-proj2 = (slice**2).sum( axis=0 )
-proj  = slice.sum( axis=0 )
-projection = proj2 / proj
-# projection = proj
-projection = np.log10( projection )
-data_sim = { 'projection':projection, 'z':current_z }
-projections[i] = data_sim
+snapshots = np.arange( 50, 501, 50 )[::-1]
+
+vmin, vmax = np.inf, -np.inf
+for i, n_snap in enumerate(snapshots):
+  file_name = input_dir + f'grid_CIC_{n_snap:03}.h5'
+  file = h5.File( file_name, 'r' )
+  current_z = file.attrs['current_z'][0]
+  density = file['density']
+  slice = density[slice_start:slice_start+slice_width, :, :]
+  slice[slice > dens_max ] = dens_max
+  slice[slice < dens_min ] = dens_min
+  proj2 = (slice**2).sum( axis=0 )
+  proj  = slice.sum( axis=0 )
+  projection = proj2 / proj
+  # projection = proj
+  projection = np.log10( projection )
+  vmax = max( vmax, projection.max() )
+  vmin = min( vmin, projection.min() )
+  print( vmin, vmax )
+  data_sim = { 'projection':projection, 'z':current_z }
+  projections[i] = data_sim
 
 
 
@@ -58,44 +67,71 @@ if system == 'Lux':      prop = matplotlib.font_manager.FontProperties( fname=os
 if system == 'Shamrock': prop = matplotlib.font_manager.FontProperties( fname=os.path.join('/home/bruno/fonts/Helvetica', "Helvetica.ttf"), size=12)
 
 
-color_map = 'inferno'
+# color_map = 'inferno'
+color_map = palettable.cmocean.sequential.Dense_20_r.mpl_colormap
+# color_map = palettable.scientific.sequential.Devon_20.mpl_colormap
+
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['mathtext.rm'] = 'serif'
+
 
 nrows, ncols = 3, 3
-fig, ax_l = plt.subplots(nrows=nrows, ncols=ncols, figsize=(4*ncols,4*nrows))
+fig, ax_l = plt.subplots(nrows=nrows, ncols=ncols, figsize=(5*ncols,4*nrows))
 plt.subplots_adjust( hspace = 0.015, wspace=0.015)
 
-for i in range(nrows):
-  for j in range(ncols):
-    fig_id = i*ncols + j
-    fig_id = 0
-    ax = ax_l[i][j]
+fig = plt.figure(figsize=(5*ncols, 4*nrows))
 
-    z = projections[fig_id]['z']
-    projection = projections[fig_id]['projection']
-    ax.imshow( projection, cmap=color_map, extent=[0,115, 0,115] )
-    
-    text_pos_x = 0.75
-    ax.text(text_pos_x, 0.95, r'$z={0:.1f}$'.format(z), horizontalalignment='left',  verticalalignment='center', transform=ax.transAxes, fontsize=figure_text_size, color=text_color) 
-    
-    ax.set_yticklabels([])
-    ax.set_xticklabels([])
-    # 
-    ax.set_axis_off()
-    
-    if black_background: 
-      fig.patch.set_facecolor('black') 
-      ax.set_facecolor('k')
-      [ spine.set_edgecolor('black') for spine in list(ax.spines.values()) ]
-      
-  
-    if i == nrows - 1 and j == 0:
-      ax.text(0.1, 0.1, r'$35 \, h^{-1}\mathrm{Mpc}$', horizontalalignment='left',  verticalalignment='center', transform=ax.transAxes, fontsize=14, color=text_color) 
-      line_start, line_width = 11.5,35 
-      line_y = 6
-      ax.plot( [line_start, line_start+line_width], [ line_y, line_y], c=text_color, lw=2)
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+grid = ImageGrid(fig, 111,          # as in plt.subplot(111)
+                 nrows_ncols=(nrows, ncols),
+                 axes_pad=0.08,
+                 share_all=True,
+                 cbar_location="right",
+                 cbar_mode="single",
+                 cbar_size="3%",
+                 cbar_pad=0.15,
+                 )
+#
+# for i in range(nrows):
+#   for j in range(ncols):
+for fig_id, ax in enumerate(grid):
+  # fig_id = i*ncols + j
+  # ax = ax_l[i][j]
+
+  z = projections[fig_id]['z']
+  projection = projections[fig_id]['projection']
+  im = ax.imshow( projection, vmax=vmax, vmin=vmin, cmap=color_map, extent=[0,115, 0,115] )
+
+  text_pos_x = 0.95
+  ax.text(text_pos_x, 0.94, r'$z={0:.1f}$'.format(z), horizontalalignment='right',  verticalalignment='center', transform=ax.transAxes, fontsize=figure_text_size, color=text_color) 
+
+  ax.set_yticks([])
+  ax.set_xticks([])
+  # 
+  # ax.set_axis_off()
+
+  if black_background: 
+    fig.patch.set_facecolor('black') 
+    ax.set_facecolor('k')
+    [ spine.set_edgecolor('black') for spine in list(ax.spines.values()) ]
 
 
-figure_name = output_dir + f'fig_density_wdm_{n_snap}.png'
+  if fig_id == 8:
+    ax.text(0.1, 0.1, r'$40 \, h^{-1}\mathrm{Mpc}$', horizontalalignment='left',  verticalalignment='center', transform=ax.transAxes, fontsize=16, color=text_color) 
+    line_start, line_width = 11.5, 40
+    line_y = 5
+    ax.plot( [line_start, line_start+line_width], [ line_y, line_y], c=text_color, lw=2)
+
+
+# Colorbar
+cbar = ax.cax.colorbar(im)
+ax.cax.toggle_label(True)
+cbar.ax.tick_params(labelsize=12, size=5, width=2, length=3, direction='in' )
+cbar.set_label_text( r'$\log_{10}  \,( \rho_{\mathrm{DM}} /   h^2 \mathrm{M_\odot } \mathrm{kpc}^{-3} ) $', fontsize=14 )
+
+figure_name = output_dir + f'fig_density_dm_{slice_start}.png'
 fig.savefig( figure_name, bbox_inches='tight', dpi=400, facecolor=fig.get_facecolor() )
 print( f'Saved Figure: {figure_name}' )
 
