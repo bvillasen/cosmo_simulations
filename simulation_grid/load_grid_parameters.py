c552903bdbe0bc069555a97188a683e11605ea08
bruno <bvillasen@gmail.com>
2021-08-24 12:23:46 -0700

    fixed bug

--- simulation_grid/load_grid_parameters.py
+++ simulation_grid/load_grid_parameters.py
@@ -7,7 +7,3 @@ from simulation_parameters import grid_name
 if grid_name == '1024_np5_nsim16':  from parameters_np5_nsim16 import param_wdm_UVB_Rates as Grid_Parameters
 if grid_name == '1024_np2_nsim16':  from parameters_np2_nsim16 import param_UVB_Rates as Grid_Parameters
 if grid_name == '1024_np4_nsim81':  from parameters_np4_nsim81 import param_UVB_Rates as Grid_Parameters
-
-
-
-\
