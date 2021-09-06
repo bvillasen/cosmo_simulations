81d920a0045378afd8daa9d0d591c209c74c94c5
bruno villasenor <bvillasen@gmail.com>
2021-09-05 19:36:27 -0700

    New 64 sim gre for wdm m=2kev

--- simulation_grid/parameter_files/parameters_mwdm2p0_nsim64.py
+++ simulation_grid/parameter_files/parameters_mwdm2p0_nsim64.py
@@ -6,14 +6,14 @@ param_UVB_Rates = {}
 param_UVB_Rates[0] = {}
 param_UVB_Rates[0]['key'] = 'A'
 param_UVB_Rates[0]['name'] = 'scale_H_ion'
-param_UVB_Rates[0]['values'] = [ 0.4, 0.8, 1.2, 1.6 ]
+param_UVB_Rates[0]['values'] = [ 1.0, 1.4, 1.8, 2.2 ]
 
 param_UVB_Rates[1] = {}
 param_UVB_Rates[1]['key'] = 'B'
 param_UVB_Rates[1]['name'] = 'scale_H_Eheat'
-param_UVB_Rates[1]['values'] = [ 0.3, 0.6, 0.9, 1.2 ]
+param_UVB_Rates[1]['values'] = [ 0.1, 0.6, 1.1, 1.6 ]
 
 param_UVB_Rates[2] = {}
 param_UVB_Rates[2]['key'] = 'C'
 param_UVB_Rates[2]['name'] = 'scale_He_Eheat'
-param_UVB_Rates[2]['values'] = [ 0.2, 0.7, 1.2, 1.7 ]
+param_UVB_Rates[2]['values'] = [ 0.2, 0.8, 1.4, 2.0]
