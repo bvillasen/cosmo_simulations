1d61c58248ec404cdf460f6273de50a1fb3779ca
bruno villasenor <bvillasen@gmail.com>
2021-12-04 11:32:52 -0800

    ics for mwdm=3.0KeV from raw enzo

--- initial_conditions/generate_ics_from_enzo_raw.py
+++ initial_conditions/generate_ics_from_enzo_raw.py
@@ -35,7 +35,7 @@ L_Mpc = int( Lbox / 1000)
 # input_dir = data_dir + f'cosmo_sims/ics/enzo/{n_points}_{L_Mpc}Mpc/'
 # output_dir = data_dir + f'cosmo_sims/ics/{n_points}_{L_Mpc}Mpc/'
 
-m_wdm = 2.0
+m_wdm = 3.0
 input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_hydro_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/raw/'
 output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/'
 
