6c1481756d1d1b30fa0cb5b41353031de3dfd953
bruno villasenor <bvillasen@gmail.com>
2022-01-05 18:55:53 -0800

    updated

--- matter_distribution/compute_density_distribution.py
+++ matter_distribution/compute_density_distribution.py
@@ -9,40 +9,9 @@ sys.path.extend(subDirectories)
 from tools import *
 from load_data import load_snapshot_data_distributed
 
-
-wdm_masses = [ 0.25, 0.5, 1.0, 2.0, 3.0 ]
-
-sim_name = 'cdm'
-
-# for wdm_mass in wdm_masses:
-
-simulation_dir = data_dir + f'cosmo_sims/rescaled_P19/wdm/1024_50Mpc_{sim_name}/'
-input_dir = simulation_dir + 'snapshot_files/'
 output_dir = data_dir + f'cosmo_sims/rescaled_P19/wdm/density_distribution_files/'
 create_directory( output_dir )
 
-# 
-# snap_ids = np.arange( 1, 95, 1, dtype=int )
-# snaps_z = []
-# for snap_id in snap_ids:
-#   file_name = input_dir + f'{snap_id}.h5.0'
-#   file = h5.File( file_name, 'r' )
-#   z = file.attrs['Current_z'][0]
-#   snaps_z.append(z)
-#   file.close()
-# snaps_z = np.array(snaps_z)
-# 
-# z_vals = [ 6, 5, 4, 3, 2, 1 ]
-# snap_indices = []
-# for z in z_vals:
-#   z_diff = np.abs( snaps_z - z )
-#   z_diff_min = z_diff.min()
-#   if z_diff_min > 0.1: print( f'WARNING: Large z_diff_min: {z_diff_min}' )
-#   snap_id = np.where( z_diff == z_diff_min )[0][0]
-#   snap_indices.append( snap_id )
-# snap_indices = np.array( snap_indices )
-# 
-# snap_ids = snap_ids[snap_indices]
 
 snap_ids = [ 1, 4, 8, 13, 23, 42 ]
 print( snap_ids )
@@ -52,26 +21,38 @@ box_size = [ Lbox, Lbox, Lbox ]
 grid_size = [ n_cells, n_cells, n_cells ] #Size of the simulation grid
 precision = np.float32
 fields = [ 'density' ]
-n_bins = 50
 
+dens_min, dens_max = -3, 4
+n_bins = 100
 data_type = 'hydro'
 
-sim_data = {}
+wdm_masses = [ 0.25, 0.5, 1.0, 2.0, 3.0 ]
 
-snap_id = snap_ids[0]
-for snap_id in snap_ids:
-  snap_data = load_snapshot_data_distributed( data_type, fields,  snap_id, input_dir,  box_size, grid_size, precision  )
-  z = snap_data['Current_z']
-  density = snap_data['density']
-  dens_mean = density.mean()
-  log_density = np.log10(density/dens_mean)
-  bin_edges = np.linspace( log_density.min(), log_density.max(), n_bins )
-  hist, bin_edges = np.histogram( log_density, bins=bin_edges )
-  distribution = hist / hist.sum()
-  bin_centers = ( bin_edges[1:] + bin_edges[:-1] ) / 2
-  sim_data[snap_id] = { 'z':z, 'bin_centers':bin_centers, 'distribution':distribution }
-  # break
-  
-file_name = output_dir + f'density_distribution_{sim_name}.pkl'
-Write_Pickle_Directory( sim_data, file_name )
+for wdm_mass in wdm_masses:
+
+  # sim_name = 'cdm'
+  sim_name = f'wdm_{wdm_mass}kev'
+  simulation_dir = data_dir + f'cosmo_sims/rescaled_P19/wdm/1024_50Mpc_{sim_name}/'
+  input_dir = simulation_dir + 'snapshot_files/'
+
+
+  sim_data = {}
+
+  snap_id = snap_ids[0]
+  for snap_id in snap_ids:
+    snap_data = load_snapshot_data_distributed( data_type, fields,  snap_id, input_dir,  box_size, grid_size, precision  )
+    z = snap_data['Current_z']
+    density = snap_data['density']
+    dens_mean = density.mean()
+    log_density = np.log10(density/dens_mean)
+    # bin_edges = np.linspace( log_density.min(), log_density.max(), n_bins )
+    bin_edges = np.linspace( dens_min, dens_max, n_bins )
+    hist, bin_edges = np.histogram( log_density, bins=bin_edges )
+    distribution = hist / hist.sum()
+    bin_centers = ( bin_edges[1:] + bin_edges[:-1] ) / 2
+    sim_data[snap_id] = { 'z':z, 'bin_centers':bin_centers, 'distribution':distribution }
+    # break
+    
+  file_name = output_dir + f'density_distribution_{sim_name}.pkl'
+  Write_Pickle_Directory( sim_data, file_name )
 
