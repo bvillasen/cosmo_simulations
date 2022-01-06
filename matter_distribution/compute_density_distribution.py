ba1fe32e915f438097817d6e7a7f35db68144a49
bruno villasenor <bvillasen@gmail.com>
2022-01-05 17:09:22 -0800

    new

--- matter_distribution/compute_density_distribution.py
+++ matter_distribution/compute_density_distribution.py
@@ -15,7 +15,7 @@ create_directory( output_dir )
 simulation_dir = data_dir + 'cosmo_sims/rescaled_P19/wdm/1024_50Mpc_cdm/'
 input_dir = simulation_dir + 'snapshot_files/'
 
-snap_ids = range( 1, 99, 1 )
+snap_ids = np.arange( 1, 99, 1, dtype=int )
 snaps_z = []
 for snap_id in snap_ids:
   file_name = input_dir + f'{snap_id}.h5.0'
