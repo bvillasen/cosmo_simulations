e63995d748ae4b5338621efb35376080c68c4889
bruno villasenor <bvillasen@gmail.com>
2022-01-06 10:07:05 -0800

    updated

--- matter_distribution/plot_density_distribution.py
+++ matter_distribution/plot_density_distribution.py
@@ -18,8 +18,6 @@ create_directory( output_dir )
 
 snap_ids = [1, 4, 8, 13, 23, 42 ]
 
-file_name = input_dir + 'density_distribution_cdm.pkl'
-sim_data = Load_Pickle_Directory( file_name )
 
 import matplotlib
 matplotlib.rcParams['font.sans-serif'] = "Helvetica"
@@ -27,7 +25,7 @@ matplotlib.rcParams['font.family'] = "sans-serif"
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['mathtext.rm'] = 'serif'
 
-label_size = 14
+label_size = 16
 figure_text_size = 14
 tick_label_size_major = 15
 tick_label_size_minor = 13
@@ -42,22 +40,54 @@ ncols, nrows = 2, 3
 figure_width = 6
 figure_height = 18
 fig, ax_l = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*figure_width,figure_height))
-plt.subplots_adjust( hspace = 0.15, wspace=0.18)
+plt.subplots_adjust( hspace = 0.15, wspace=0.2)
+
+wdm_masses = [ 0.25, 0.5, 1.0, 2.0, 3.0 ]
+
+sim_names = [ ]
+for wdm_mass in wdm_masses:
+  sim_names.append( f'wdm_m{wdm_mass}kev' )
+sim_names.append('cdm')
 
 for i in range(nrows):
   for j in range(ncols):
     fig_id = j + i*ncols
     snap_id = snap_ids[fig_id]
-    
-    snap_data = sim_data[snap_id]
-    z = snap_data['z']
-    bin_centers = snap_data['bin_centers']
-    distribution = snap_data['distribution']
-    
     ax = ax_l[i][j]
-    ax.plot( bin_centers, distribution )
     
-    ax.text(0.9, 0.93, r'$z=${0:.1f}'.format(np.round(z)), horizontalalignment='center',  verticalalignment='center', transform=ax.transAxes, fontsize=figure_text_size, color=text_color) 
+    for sim_id,sim_name in enumerate(sim_names):
+      file_name = input_dir + f'density_distribution_{sim_name}.pkl'
+      sim_data = Load_Pickle_Directory( file_name )
+
+      snap_data = sim_data[snap_id]
+      z = snap_data['z']
+      bin_centers = snap_data['bin_centers']
+      distribution = snap_data['distribution']
+      if sim_name == 'cdm': label = 'CDM'
+      else:
+        wdm_mass = wdm_masses[sim_id]
+        label = f'WDM {wdm_mass} keV'
+       
+      ax.plot( bin_centers, distribution, label=label )
+    
+    ax.text(0.1, 0.95, r'$z=${0:.1f}'.format(np.round(z)), horizontalalignment='center',  verticalalignment='center', transform=ax.transAxes, fontsize=figure_text_size, color=text_color) 
+
+    leg = ax.legend(  loc=1, frameon=False, fontsize=legend_font_size    )
+    
+    
+    ax.set_xlim( -2.5, 4 )
+    # ax.set_ylim( 0, .1 )
+    
+    # ax.set_ylim( 10**-8, 1 )
+    # ax.set_yscale('log')
+    
+    ax.set_ylabel( r' $P\,(\Delta)$', fontsize=label_size, color= text_color )
+    ax.set_xlabel( r'$ \mathrm{log_{10}} \, \Delta$', fontsize=label_size, color= text_color )
+
+
+    ax.tick_params(axis='both', which='major', color=text_color, labelcolor=text_color, labelsize=tick_label_size_major, size=tick_size_major, width=tick_width_major, direction='in' )
+    ax.tick_params(axis='both', which='minor', color=text_color, labelcolor=text_color, labelsize=tick_label_size_minor, size=tick_size_minor, width=tick_width_minor, direction='in')
+
 
 
 
