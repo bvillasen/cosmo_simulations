4641886210ec53dd3a06fbfaebc20c78b4ad86bb
bruno villasenor <bvillasen@gmail.com>
2022-01-06 10:32:04 -0800

    new

--- matter_distribution/compute_power_spectrum.py
+++ matter_distribution/compute_power_spectrum.py
@@ -10,7 +10,7 @@ from tools import *
 from load_data import load_snapshot_data_distributed
 from power_spectrum_functions import get_power_spectrum
 
-output_dir = data_dir + f'cosmo_sims/rescaled_P19/wdm/density_distribution_files/'
+output_dir = data_dir + f'cosmo_sims/rescaled_P19/wdm/power_spectrum_files/'
 create_directory( output_dir )
 
 data_type = 'hydro'
@@ -42,18 +42,15 @@ input_dir = simulation_dir + 'snapshot_files/'
 sim_data = {}
 
 snap_id = snap_ids[0]
-# for snap_id in snap_ids:
-snap_data = load_snapshot_data_distributed( data_type, fields,  snap_id, input_dir,  box_size, grid_size, precision  )
-z = snap_data['Current_z']
-density = snap_data['density']
-print( f'Computing Power Spectrum  snap_id: {snap_id}  z:{z}' )
-power_spectrum, k_vals, count = get_power_spectrum( density, Lbox, nx, ny, nz, dx, dy, dz,  n_kSamples=n_bins )
-
-
-
-# sim_data[snap_id] = { 'z':z, 'bin_centers':bin_centers, 'distribution':distribution }
+for snap_id in snap_ids:
+  snap_data = load_snapshot_data_distributed( data_type, fields,  snap_id, input_dir,  box_size, grid_size, precision  )
+  z = snap_data['Current_z']
+  density = snap_data['density']
+  print( f'Computing Power Spectrum  snap_id: {snap_id}  z:{z}' )
+  power_spectrum, k_vals, n_in_bin = get_power_spectrum( density, Lbox, nx, ny, nz, dx, dy, dz,  n_kSamples=n_bins )
+  sim_data[snap_id] = { 'z':z, 'k_vals':k_vals, 'power_spectrum':power_spectrum, 'n_in_bin':n_in_bin }
   # break
 
-# file_name = output_dir + f'power_spectrum_{sim_name}_{data_type}.pkl'
-# Write_Pickle_Directory( sim_data, file_name )
+file_name = output_dir + f'power_spectrum_{sim_name}_{data_type}.pkl'
+Write_Pickle_Directory( sim_data, file_name )
 
