b19e42f6ab6cc7d3daae469eaa4ada196eec2203
bruno villasenor <bvillasen@gmail.com>
2022-01-05 16:51:38 -0800

    updated

--- matter_distribution/compute_density_distribution.py
+++ matter_distribution/compute_density_distribution.py
@@ -7,3 +7,10 @@ root_dir = os.path.dirname(os.getcwd()) + '/'
 subDirectories = [x[0] for x in os.walk(root_dir)]
 sys.path.extend(subDirectories)
 from tools import *
+
+
+output_dir = data_dir + 'cosmo_sims/rescaled_P19/wdm/figures/'
+create_directory( output_dir )
+
+simulation_dir = data_dir + 'cosmo_sims/rescaled_P19/wdm/1024_50Mpc_cdm/'
+input_dir = simulation_dir + 'snapshot_files/'
