4d2ea91befbb22e90ce72b0b73f438ebb8da6fe5
bruno villasenor <bvillasen@gmail.com>
2022-01-05 17:07:40 -0800

    new

--- matter_distribution/compute_density_distribution.py
+++ matter_distribution/compute_density_distribution.py
@@ -31,7 +31,7 @@ for z in z_vals:
   z_diff = np.abs( snaps_z - z )
   z_diff_min = z_diff.min()
   if z_diff_min > 0.1: print( f'WARNING: Large z_diff_min: {z_diff_min}' )
-  snap_id = np.where( z_diff == z_diff_min )[0]
+  snap_id = np.where( z_diff == z_diff_min )[0][0]
   snap_indices.append( snap_id )
 
 snap_ids = snap_ids[snap_indices]
