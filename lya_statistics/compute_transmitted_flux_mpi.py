d9f287e788bff9a167e721993ef0325dd286e31d
bruno villasenor <bvillasen@gmail.com>
2022-01-10 15:08:34 -0800

    new

--- lya_statistics/compute_transmitted_flux_mpi.py
+++ lya_statistics/compute_transmitted_flux_mpi.py
@@ -67,16 +67,16 @@ cosmology['Omega_L'] = 0.6889
 axis_list = [ 'x', 'y', 'z' ]
 n_skewers_list = [ 'all', 'all', 'all']
 skewer_ids_list = [ 'all', 'all', 'all']
-field_list = [  'HI_density', 'los_velocity', 'temperature' ]
+fields_to_load = [  'HI_density', 'los_velocity', 'temperature' ]
 
 # for n_file in local_snaps:
 n_file = 56
-skewer_dataset = load_skewers_multiple_axis( axis_list, n_skewers_list, n_file, input_dir, ids_to_load_list=skewer_ids_list) 
-current_z = skewer_dataset['current_z']
-print( f'current_z: {current_z}')
-cosmology['current_z'] = current_z
-skewers_data = { field:skewer_dataset[field] for field in field_list }
-data_Flux = Compute_Skewers_Transmitted_Flux( skewers_data, cosmology, box )
+skewer_dataset = Load_Skewers_File( n_file, input_dir, axis_list=axis_list, fields_to_load=fields_to_load ):
+# current_z = skewer_dataset['current_z']
+# print( f'current_z: {current_z}')
+# cosmology['current_z'] = current_z
+# skewers_data = { field:skewer_dataset[field] for field in field_list }
+# data_Flux = Compute_Skewers_Transmitted_Flux( skewers_data, cosmology, box )
 
 # out_file_name = output_dir + f'lya_flux_{n_file:03}.h5'
 # file = h5.File( out_file_name, 'w' )
