fca64cd9bd3c8fe50f0beb8d469fa92599228442
bruno villasenor <bvillasen@gmail.com>
2022-01-20 16:20:28 -0800

    new

--- tests/compare_slices.py
+++ tests/compare_slices.py
@@ -37,17 +37,48 @@ slice_start, slice_depth = 0, 256
 
 n_snapshot = 10 
 
-slices = {} 
-data_0 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_0, box_size, grid_size,  precision, show_progess=True )
-data_1 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_1, box_size, grid_size,  precision, show_progess=True )
-z_0 = data_0['Current_z']
-z_1 = data_1['Current_z']
-if np.abs( z_0 - z_1 ) > 1e-3:
-  print( 'ERROR: Redshift of snapshots does not match')
-  exit(-1)  
+# slices = {} 
+# data_0 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_0, box_size, grid_size,  precision, show_progess=True )
+# data_1 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_1, box_size, grid_size,  precision, show_progess=True )
+# z_0 = data_0['Current_z']
+# z_1 = data_1['Current_z']
+# if np.abs( z_0 - z_1 ) > 1e-3:
+#   print( 'ERROR: Redshift of snapshots does not match')
+#   exit(-1)  
+# 
+# for field in fields:
+#   if field not in slices: slices[field] = {}
+#   slices[field][0] = data_0[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
+#   slices[field][1] = data_1[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
+# 
   
-for field in fields:
-  if field not in slices: slices[field] = {}
-  slices[field][0] = data_0[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
-  slices[field][1] = data_1[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
+n_fields = len( fields )
+
+label_size = 16
+figure_text_size = 14
+tick_label_size_major = 15
+tick_label_size_minor = 13
+tick_size_major = 5
+tick_size_minor = 3
+tick_width_major = 1.5
+tick_width_minor = 1
+text_color = 'black'
+legend_font_size = 14
+
+ncols, nrows = 3, n_fields
+figure_width = 6
+figure_height = 6
+fig, ax_l = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*figure_width, nrows*figure_height))
+plt.subplots_adjust( hspace = 0.15, wspace=0.2)
+
+
+
+
+
+figure_name = output_dir + f'slices_comparison_{n_snapshot}.png'
+fig.savefig( figure_name, bbox_inches='tight', dpi=300, facecolor=fig.get_facecolor() )
+print( f'Saved Figure: {figure_name}' )
+
+
+
   
