bee22df47cd02dede91966114c8fda699e71c8f8
bruno villasenor <bvillasen@gmail.com>
2022-01-20 18:53:48 -0800

    new

--- tests/compare_slices.py
+++ tests/compare_slices.py
@@ -20,11 +20,14 @@ input_dir_1 = sim_dir + 'snapshot_files_cholla/'
 output_dir  = sim_dir + 'figures/slices/'
 create_directory( output_dir ) 
 
+slice_start, slice_depth = 0, 256
+
 precision = np.float64
 Lbox = 50000.0    #kpc/h
 n_cells = 1024
 box_size = [ Lbox, Lbox, Lbox ]
 grid_size = [ n_cells, n_cells, n_cells ] #Size of the simulation grid
+subgrid = [ [slice_start, slice_start+slice_depth], [0, n_cells], [0, n_cells]]
 
 
 data_type = 'hydro'
@@ -33,7 +36,6 @@ data_type = 'hydro'
 fields = [ 'density', 'temperature', 'HI_density' ]
 diff = {}
 
-slice_start, slice_depth = 0, 256
 
 
 snapshots = range( 1, 16 )
@@ -42,8 +44,8 @@ n_snapshot = 10
 for n_snapshot in snapshots:
 
   slices = {} 
-  data_0 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_0, box_size, grid_size,  precision, show_progess=True )
-  data_1 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_1, box_size, grid_size,  precision, show_progess=True )
+  data_0 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_0, box_size, grid_size, subgrid=subgrid, precision, show_progess=True )
+  data_1 = load_snapshot_data_distributed( data_type, fields, n_snapshot, input_dir_1, box_size, grid_size, subgrid=subgrid, precision, show_progess=True )
   z_0 = data_0['Current_z']
   z_1 = data_1['Current_z']
   if np.abs( z_0 - z_1 ) > 1e-3:
@@ -52,8 +54,10 @@ for n_snapshot in snapshots:
 
   for field in fields:
     if field not in slices: slices[field] = {}
-    slices[field][0] = data_0[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
-    slices[field][1] = data_1[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
+    # slices[field][0] = data_0[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth
+    # slices[field][1] = data_1[field][slice_start:slice_start+slice_depth, :, :].sum( axis=0 ) / slice_depth    
+    slices[field][0] = data_0[field].sum( axis=0 ) / slice_depth
+    slices[field][1] = data_1[field].sum( axis=0 ) / slice_depth
     
   n_fields = len( fields )
 
