0f66686154667fecb943f0ae2d143d6739e74a99
bruno villasenor <bvillasen@gmail.com>
2022-01-25 13:06:52 -0800

    new

--- lya_statistics/compute_transmitted_flux_grid.py
+++ lya_statistics/compute_transmitted_flux_grid.py
@@ -137,7 +137,7 @@ for file_id in local_indices:
   cosmology['current_z'] = skewer_dataset['current_z']
   
   skewers_data = { field:skewer_dataset[field] for field in field_list }
-  print_string = f'  file  {file_id:04} / {n_total_files}.'
+  print_string = f'  file  {file_id:04} / {n_total_files}.  '
   data_Flux = Compute_Skewers_Transmitted_Flux( skewers_data, cosmology, box, print_string=print_string )
    
   # file = h5.File( out_file_name, 'w' )
@@ -146,7 +146,7 @@ for file_id in local_indices:
   # file.create_dataset( 'vel_Hubble', data=data_Flux['vel_Hubble'] )
   # file.create_dataset( 'skewers_Flux', data=data_Flux['skewers_Flux'] )
   # file.close()
-  print( f'Saved File: {out_file_name}')
+  # print( f'Saved File: {out_file_name}')
 
 
 
