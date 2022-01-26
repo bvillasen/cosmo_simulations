335100f39ff376daa43634ff6e35eaf6831e9c19
bruno villasenor <bvillasen@gmail.com>
2022-01-25 17:03:16 -0800

    new

--- lya_statistics/compute_transmitted_flux_mpi_grid.py
+++ lya_statistics/compute_transmitted_flux_mpi_grid.py
@@ -18,7 +18,7 @@ from flux_power_spectrum import Compute_Flux_Power_Spectrum
 
 args = sys.argv
 
-use_mpi = True
+use_mpi = False
 if use_mpi:
   from mpi4py import MPI
   comm = MPI.COMM_WORLD
@@ -42,6 +42,7 @@ if grid_dir[-1] != '/': grid_dir += '/'
 skewers_dir = grid_dir + 'skewers_files/'
 transmitted_flux_dir = grid_dir + 'transmitted_flux/'
 ps_dir = grid_dir + 'flux_power_spectrum/'
+analysis_dir = grid_dir + 'analysis_files/'
 grid_skewers_file_name = grid_dir + 'grid_skewers_files.pkl'
 
 selected_file_indices = [ 25, 29, 33 ] # redshits 5.0, 4.6 and 4.2
@@ -120,7 +121,6 @@ for file_id in local_indices:
   file_indx = file_data['file_indx']
   input_dir = skewers_dir + sim_dir + '/'
   flux_dir = transmitted_flux_dir + sim_dir + '/'
-  ps_sim_dir = ps_dir + sim_dir + '/'
   if not os.path.isdir(input_dir):
     print( f'ERROR: Directory not found {input_dir}' )
     continue  
@@ -133,54 +133,91 @@ for file_id in local_indices:
   flux_file_name = flux_dir + f'lya_flux_{file_indx:03}.h5'
   flux_file_exists = False
   if os.path.isfile( flux_file_name ):  flux_file_exists = True
-  
-  if not flux_file_exists:  
-    skewer_dataset = Load_Skewers_File( file_indx, input_dir, axis_list=axis_list, fields_to_load=field_list )
-    current_z = skewer_dataset['current_z']
+  if flux_file_exists: continue
     
-    # Cosmology parameters
-    cosmology = {}
-    cosmology['H0'] = skewer_dataset['H0']
-    cosmology['Omega_M'] = skewer_dataset['Omega_M']
-    cosmology['Omega_L'] = skewer_dataset['Omega_L']
-    cosmology['current_z'] = skewer_dataset['current_z']
+  skewer_dataset = Load_Skewers_File( file_indx, input_dir, axis_list=axis_list, fields_to_load=field_list )
+  current_z = skewer_dataset['current_z']
   
-    skewers_data = { field:skewer_dataset[field] for field in field_list }
-    data_Flux = Compute_Skewers_Transmitted_Flux( skewers_data, cosmology, box, print_string=print_string )
-    
-    file = h5.File( flux_file_name, 'w' )
-    file.attrs['current_z'] = current_z
-    file.attrs['Flux_mean'] = data_Flux['Flux_mean']
-    file.create_dataset( 'vel_Hubble', data=data_Flux['vel_Hubble'] )
-    file.create_dataset( 'skewers_Flux', data=data_Flux['skewers_Flux'] )
-    file.close()
-    # print( f'Saved File: {out_file_name}')
-  
-  if flux_file_exists:
-    file = h5.File( flux_file_name, 'r' )
-    current_z = file.attrs['current_z']
-    Flux_mean = file.attrs['Flux_mean']
-    vel_Hubble   = file['vel_Hubble'][...]
-    skewers_Flux = file['skewers_Flux'][...]
-    file.close()
-    data_Flux = { 'vel_Hubble':vel_Hubble, 'skewers_Flux':skewers_Flux }
-  
-  
-  if compute_ps:
-    ps_file_name = ps_sim_dir + f'flux_ps_{file_indx:03}.h5'
-    ps_file_exists = False
-    if os.path.isfile( ps_file_name ):  ps_file_exists = True
-    
-    if not ps_file_exists:
-        
-      data_ps = Compute_Flux_Power_Spectrum( data_Flux, print_string=print_string )
-      
-      file = h5.File( ps_file_name, 'w' )
-      file.attrs['current_z'] = current_z
-      file.create_dataset( 'k_vals', data=data_ps['k_vals'] )
-      file.create_dataset( 'ps_mean', data=data_ps['mean'] )
-      file.create_dataset( 'skewers_ps', data=data_ps['skewers_ps'] )
-      file.close()
-  
-  
+  # Cosmology parameters
+  cosmology = {}
+  cosmology['H0'] = skewer_dataset['H0']
+  cosmology['Omega_M'] = skewer_dataset['Omega_M']
+  cosmology['Omega_L'] = skewer_dataset['Omega_L']
+  cosmology['current_z'] = skewer_dataset['current_z']
 
+  skewers_data = { field:skewer_dataset[field] for field in field_list }
+  data_Flux = Compute_Skewers_Transmitted_Flux( skewers_data, cosmology, box, print_string=print_string )
+  
+  file = h5.File( flux_file_name, 'w' )
+  file.attrs['current_z'] = current_z
+  file.attrs['Flux_mean'] = data_Flux['Flux_mean']
+  file.create_dataset( 'vel_Hubble', data=data_Flux['vel_Hubble'] )
+  file.create_dataset( 'skewers_Flux', data=data_Flux['skewers_Flux'] )
+  file.close()
+  # print( f'Saved File: {out_file_name}')
+
+if use_mpi: comm.Barrier()
+if rank == 0: print('')
+
+# Now compute the flux power spectrum
+for file_id in local_indices:
+  if not compute_ps: continue
+    
+  file_data = skewers_files_data[file_id]
+  sim_dir = file_data['sim_dir']
+  file_indx = file_data['file_indx']
+  flux_dir = transmitted_flux_dir + sim_dir + '/'
+  ps_sim_dir = ps_dir + sim_dir + '/'
+  print_string = f'  file  {file_id:04} / {n_total_files}.  '
+  
+  flux_file_name = flux_dir + f'lya_flux_{file_indx:03}.h5'      
+  file = h5.File( flux_file_name, 'r' )
+  current_z = file.attrs['current_z']
+  Flux_mean = file.attrs['Flux_mean']
+  vel_Hubble   = file['vel_Hubble'][...]
+  skewers_Flux = file['skewers_Flux'][...]
+  file.close()
+  data_Flux = { 'vel_Hubble':vel_Hubble, 'skewers_Flux':skewers_Flux }
+
+  ps_file_name = ps_sim_dir + f'flux_ps_{file_indx:03}.h5'
+  ps_file_exists = False
+  if os.path.isfile( ps_file_name ):  ps_file_exists = True
+  if ps_file_exists: continue
+  
+  data_ps = Compute_Flux_Power_Spectrum( data_Flux, print_string=print_string )
+  file = h5.File( ps_file_name, 'w' )
+  file.attrs['current_z'] = current_z
+  file.create_dataset( 'k_vals', data=data_ps['k_vals'] )
+  file.create_dataset( 'ps_mean', data=data_ps['mean'] )
+  file.create_dataset( 'skewers_ps', data=data_ps['skewers_ps'] )
+  file.close()
+
+if use_mpi: comm.Barrier()
+if rank != 0: exit()
+
+# Now compare the Power Spectrum to the one computed in-the-fly
+for file_id in file_indices:
+  if not compute_ps: continue
+    
+  file_data = skewers_files_data[file_id]
+  sim_dir = file_data['sim_dir']
+  file_indx = file_data['file_indx']
+  ps_sim_dir = ps_dir + sim_dir + '/'
+  analysis_sim_dir = analysis_dir + sim_dir + '/'
+  print_string = f'  file  {file_id:04} / {n_total_files}.  '
+
+  ps_file_name = ps_sim_dir + f'flux_ps_{file_indx:03}.h5'
+  file = h5.File( ps_file_name, 'r' )
+  current_z = file.attrs['current_z']
+  k_vals = file['k_vals'][...] 
+  ps_mean = file['ps_mean'][...]
+  file.close()
+  
+  analysis_file_name = analysis_dir + f'{file_indx}_analysis.h5'
+  file = h5.File( analysis_file_name, 'r' )
+  
+  
+  
+  break
+  
+  
