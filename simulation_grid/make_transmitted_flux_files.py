b2b8d47c173258b21b9b2c70718ff939c71844b9
bruno villasenor <bvillasen@gmail.com>
2022-02-10 19:21:30 -0800

    new

--- simulation_grid/make_transmitted_flux_files.py
+++ simulation_grid/make_transmitted_flux_files.py
@@ -12,7 +12,7 @@ grid_dir = data_dir + 'cosmo_sims/sim_grid/grid_thermal/1024_P19m_np4_nsim400/'
 sim_dirs = [ dir for dir in os.listdir(grid_dir) if os.path.isdir(grid_dir+dir) and dir[0]=='S' ]
 sim_dirs.sort()
 
-file_indx = 56
+file_indx = 55
 sim_dir = sim_dirs[0]
 print(f'Dir: {sim_dir}')
 file_name = grid_dir + sim_dir + f'/analysis_files/{file_indx}_analysis.h5'
