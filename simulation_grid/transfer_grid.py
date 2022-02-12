6ec56f4c0bca4391a3e68b1140f90444ab54da89
bruno villasenor <bvillasen@gmail.com>
2022-02-11 17:58:03 -0800

    updated transfer

--- simulation_grid/transfer_grid.py
+++ simulation_grid/transfer_grid.py
@@ -19,8 +19,11 @@ sim_grid_dir = data_dir + 'cosmo_sims/sim_grid/'
 # if wdm_mass == 6.0: src_grid_dir = sim_grid_dir + '1024_mwdm6p0_nsim64/' 
 # dst_grid_dir = sim_grid_dir + '1024_wdmgrid_nsim320/'
 
-src_grid_dir = sim_grid_dir + '1024_wdmgrid_nsim200_deltaZ_n0p5/'
-dst_grid_dir = sim_grid_dir + '1024_wdmgrid_nsim600/'
+# src_grid_dir = sim_grid_dir + '1024_wdmgrid_nsim200_deltaZ_n0p5/'
+# dst_grid_dir = sim_grid_dir + '1024_wdmgrid_nsim600/'
+
+src_grid_dir = sim_grid_dir + '1024_wdmgrid_nsim600/'
+dst_grid_dir = sim_grid_dir + '1024_wdmgrid_cdm/'
 
 # constant_params = { 'wdm_mass': wdm_mass }
 constant_params = None
@@ -37,7 +40,8 @@ print( f'N to transfer: {n_to_transfer} ' )
 # directories_to_copy = [ 'analysis_files', 'simulation_files' ]
 # directories_to_copy = [ 'skewers_files' ] 
 # directories_to_copy = [ 'transmitted_flux', 'flux_power_spectrum' ]
-directories_to_copy = [ 'flux_power_spectrum' ] 
+# directories_to_copy = [ 'flux_power_spectrum' ] 
+directories_to_copy = [ 'analysis_files', 'simulation_files', 'flux_power_spectrum' ] 
 
 # directories_to_move = [ 'snapshot_files' ]
 directories_to_move = []
