d5c8b4184477d9aef7592987b8e35fccc1d69bfd
bruno villasenor <bvillasen@gmail.com>
2022-02-14 15:15:44 -0800

    new

--- papers/wdm_2021/plot_temperature_evolution.py
+++ papers/wdm_2021/plot_temperature_evolution.py
@@ -36,42 +36,41 @@ for sim_id,file_id in enumerate(selected_files):
   data_all[sim_id] = { 'z':z, 'T0':T0 }
 
 
-# nrows = 1
-# ncols = 1
-# 
-# tick_size_major, tick_size_minor = 6, 4
-# tick_label_size_major, tick_label_size_minor = 14, 12
-# tick_width_major, tick_width_minor = 1.5, 1
-# 
-# font_size = 18
-# label_size = 16
-# alpha = 0.7
-# 
-# line_width = 0.6
-# 
-# border_width = 1.5
-# 
-# text_color  = 'black'
-# 
-# 
-# fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8*ncols,6*nrows))
-# 
-# 
-# for sim_id in sim_data_all:
-#   sim_data = sim_data_all[sim_id]
-#   z = sim_data['z']
-#   T0 = sim_data['T0']
-#   label = sim_data['label']
-#   ax.plot( z, T0, label=label)
-# 
-# ax.legend( frameon=False)
-# 
-# 
-# ax.set_xlim( 2, 8 )
-# # ax.set_xlims( 2, 8 )
-# 
-# 
-# figure_name = output_dir + 'temperature_wdm_simulations.png'
-# fig.savefig( figure_name, bbox_inches='tight', dpi=300, facecolor=fig.get_facecolor() )
-# print( f'Saved Figure: {figure_name}' )
-# 
+nrows = 1
+ncols = 1
+
+tick_size_major, tick_size_minor = 6, 4
+tick_label_size_major, tick_label_size_minor = 14, 12
+tick_width_major, tick_width_minor = 1.5, 1
+
+font_size = 18
+label_size = 16
+alpha = 0.7
+
+line_width = 0.6
+
+border_width = 1.5
+
+text_color  = 'black'
+
+
+fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8*ncols,6*nrows))
+
+
+for sim_id in data_all:
+  sim_data = data_all[sim_id]
+  z = sim_data['z']
+  T0 = sim_data['T0']
+  label = ''
+  ax.plot( z, T0, label=label)
+
+ax.legend( frameon=False)
+
+
+ax.set_xlim( 2, 8 )
+# ax.set_xlims( 2, 8 )
+
+
+figure_name = output_dir + 'temperature_wdm_chains.png'
+fig.savefig( figure_name, bbox_inches='tight', dpi=300, facecolor=fig.get_facecolor() )
+print( f'Saved Figure: {figure_name}' )
