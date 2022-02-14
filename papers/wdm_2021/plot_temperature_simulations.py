af4cb043dd889232693e23c07aa6a7c79fa1d6e6
bruno villasenor <bvillasen@gmail.com>
2022-02-14 10:45:15 -0800

    new

--- papers/wdm_2021/plot_temperature_simulations.py
+++ papers/wdm_2021/plot_temperature_simulations.py
@@ -11,8 +11,19 @@ from tools import *
 
 base_dir = data_dir + 'rescaled_P19/wdm/'
 
-sim_name = '1024_50Mpc_cdm'
+n_files = 56
 
+sim_name = '1024_50Mpc_cdm'
 input_dir = base_dir + f'{sim_name}/analysis_files/'
+
+for n_file in range(n_files):
+  
+  file_name = input_dir + f'{n_file}_analysis.h5'
+  print( f'Loading File: {file_name}' )
+  file = h5.File( file_name, 'r')
+  
+  
+  break 
+  
 
 
