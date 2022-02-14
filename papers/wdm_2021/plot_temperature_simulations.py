9c83878e0c88e2fe53eb7298ee41c848e87dac2b
bruno villasenor <bvillasen@gmail.com>
2022-02-14 12:16:43 -0800

    new

--- papers/wdm_2021/plot_temperature_simulations.py
+++ papers/wdm_2021/plot_temperature_simulations.py
@@ -18,28 +18,28 @@ for m_wdm in wdm_masses:
 sim_names.append('1024_50Mpc_cdm')
 
 
-# n_files = 56
-# sim_data_all = {}
-# for sim_id, sim_name in sim_names:
-#   sim_name = '1024_50Mpc_cdm'
-#   input_dir = base_dir + f'{sim_name}/analysis_files/'
-# 
-#   z_vals, T0_vals = [], []
-#   for n_file in range(n_files):
-# 
-#     file_name = input_dir + f'{n_file}_analysis.h5'
-#     print( f'Loading File: {file_name}' )
-#     file = h5.File( file_name, 'r')
-#     z = file.attrs['current_z'][0]
-#     file.close()
-# 
-#     file_name = input_dir + f'fit_mcmc_delta_0_1.0/fit_{n_file}.pkl'
-#     fit_data = Load_Pickle_Directory( file_name, 'r' )
-#     T0 = fit_data['T0']['mean']
-#     z_vals.appens(z)
-#     T0_vals.append(T0)
-#   sim_data_all[sim_id] = {'z':z_vals, 'T0':T0_vals }
-# 
-# 
+n_files = 56
+sim_data_all = {}
+for sim_id, sim_name in sim_names:
+  sim_name = '1024_50Mpc_cdm'
+  input_dir = base_dir + f'{sim_name}/analysis_files/'
+
+  z_vals, T0_vals = [], []
+  for n_file in range(n_files):
+
+    file_name = input_dir + f'{n_file}_analysis.h5'
+    print( f'Loading File: {file_name}' )
+    file = h5.File( file_name, 'r')
+    z = file.attrs['current_z'][0]
+    file.close()
+
+    file_name = input_dir + f'fit_mcmc_delta_0_1.0/fit_{n_file}.pkl'
+    fit_data = Load_Pickle_Directory( file_name, 'r' )
+    T0 = fit_data['T0']['mean']
+    z_vals.appens(z)
+    T0_vals.append(T0)
+  sim_data_all[sim_id] = {'z':z_vals, 'T0':T0_vals }
+
+
 
 
