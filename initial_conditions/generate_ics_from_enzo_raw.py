6700582e96a1730756ae56f67b999ebf987adaa4
bruno villasenor <bvillasen@gmail.com>
2022-02-17 21:40:25 -0800

    updated

--- initial_conditions/generate_ics_from_enzo_raw.py
+++ initial_conditions/generate_ics_from_enzo_raw.py
@@ -28,21 +28,21 @@ if 'particles' in types: ics_particles = True
 
 
 # Box Size
-# Lbox = 50000.0    #kpc/h
-# n_points = 2048
-# n_boxes  = 512
-# L_Mpc = int( Lbox / 1000)
-
-# Box Size
-Lbox = 25000.0    #kpc/h
-n_points = 1024
-n_boxes  = 128
+Lbox = 50000.0    #kpc/h
+n_points = 2048
+n_boxes  = 512
 L_Mpc = int( Lbox / 1000)
+
+# # Box Size
+# Lbox = 25000.0    #kpc/h
+# n_points = 1024
+# n_boxes  = 128
+# L_Mpc = int( Lbox / 1000)
 
 # input_dir = data_dir + f'cosmo_sims/ics/enzo/{n_points}_{L_Mpc}Mpc_dmo/'
 # output_dir = data_dir + f'cosmo_sims/ics/{n_points}_{L_Mpc}Mpc_dmo/'
 
-m_wdm = 80.0
+m_wdm = 1.0
 input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_hydro_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/raw/'
 output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/'
 
