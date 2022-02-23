2876c0a89f3756d25e81a82ec165be207ebff750
bruno villasenor <bvillasen@gmail.com>
2022-02-22 18:09:47 -0800

    updated

---
+++ simulation_grid/modify_files_from_grid.py
@@ -0,0 +1,14 @@
+import sys, os, time
+import numpy
+from filecmp import dircmp
+from shutil import copyfile, copytree, move, rmtree
+base_dir = os.path.dirname(os.getcwd()) + '/'
+subDirectories = [x[0] for x in os.walk(base_dir)]
+sys.path.extend(subDirectories)
+from tools import *
+from transfer_grid_functions import *
+
+grid_dir = data_dir + 'cosmo_sims/sim_grid/1024_wdmgrid_nsim900/'
+dirs_in_grid = [ d for d in os.listdir(grid_dir) if os.path.isdir(d) ]
+
+
