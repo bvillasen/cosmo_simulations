8eeff5d4269ece31288c5a5daeda060cc84220ff
bruno villasenor <bvillasen@gmail.com>
2022-02-24 09:55:08 -0800

    new

--- simulation_grid/modify_files_from_grid.py
+++ simulation_grid/modify_files_from_grid.py
@@ -14,13 +14,16 @@ dirs_in_grid = [ d for d in os.listdir(grid_dir) if os.path.isdir(grid_dir+d) ]
 base_dir = grid_dir + 'simulation_files/'
 sim_dirs = [ d for d in os.listdir(base_dir) if os.path.isdir(base_dir+d) ]
 
+files_to_delete = []
 for sim_dir in sim_dirs:
   src_dir = base_dir + f'{sim_dir}/'
   files = [ f for f in os.listdir(src_dir) if os.path.isfile(src_dir+f) and f.find('core')==0 ]
   if len(files) > 0:
     for file in files:
       file_name = src_dir + file
-      print( f'Deleting file: {file_name}' )
-      # os.remove(file_name)
-
-
+      files_to_delete.append(file_name)
+      
+print(f'Files to delete: {files_to_delete}' )
+print( f'N to delete: {len(files_to_delete)}' )      
+for file_name in files_to_delete:
+  os.remove(file_name)
