8c606e2ceb3cfd6740c5e6dcc5fe50ec9fef165a
bruno villasenor <bvillasen@gmail.com>
2022-02-24 13:16:31 -0800

    updated

--- lya_statistics/compute_transmitted_flux_mpi_grid.py
+++ lya_statistics/compute_transmitted_flux_mpi_grid.py
@@ -92,7 +92,9 @@ if rank == 0:
     for file_indx in file_indices:
       file_name = f'{skewers_dir}{sim_dir}/{file_indx}_skewers.h5'
       is_file = os.path.isfile( file_name )
-      if not is_file: print(f'ERROR: File not found {file_name}' )
+      if not is_file: 
+        print(f'WARNING: File not found {file_name}' )
+        continue
       skewers_files_data[file_id] = { 'sim_id': sim_id, 'sim_dir':sim_dir, 'file_indx':file_indx, 'file_name':file_name }
       file_id += 1
   
