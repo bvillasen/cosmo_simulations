2363ce66a2c0a9231c60b92202fbc2085da7a596
bruno villasenor <bvillasen@gmail.com>
2022-02-24 17:25:07 -0800

    updated

--- simulation_grid/fit_mcmc_wdm_custom_cov.py
+++ simulation_grid/fit_mcmc_wdm_custom_cov.py
@@ -116,7 +116,7 @@ params = SG.parameters
 stats_file   = output_dir + 'fit_mcmc.pkl'
 samples_file = output_dir + 'samples_mcmc.pkl'
 
-nIter = 5000000 
+nIter = 5000000 * 2
 nBurn = nIter // 10
 nThin = 1
 model, params_mcmc = get_mcmc_model( comparable_data, comparable_grid, fields_to_fit, 'mean', SG, error_type=error_type )
