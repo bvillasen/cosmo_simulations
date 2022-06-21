376752109d9cef2401d6a31a77df5566ecd7d1db
bruno villasenor <bvillasen@gmail.com>
2022-06-20 21:14:36 -0700

    updated

--- matter_distribution/compute_power_spectrum_wdm_dens_ceil.py
+++ matter_distribution/compute_power_spectrum_wdm_dens_ceil.py
@@ -58,29 +58,36 @@ print( f'rank: {rank}  delta_local:{delta_max_local}' )
 
 snap_id = 6
 
-snap_data = load_snapshot_data_distributed( data_type, fields,  snap_id, input_dir,  box_size, grid_size, precision  )
-z = snap_data['Current_z']
-density = snap_data['density']
-rho_mean = density.mean()
-nx, ny, nz = density.shape
-n_total = nx * ny * nz
+for snap_id in [ 6, 7, 8]:
+
+  snap_data = load_snapshot_data_distributed( data_type, fields,  snap_id, input_dir,  box_size, grid_size, precision  )
+  z = snap_data['Current_z']
+  density = snap_data['density']
+  rho_mean = density.mean()
+  nx, ny, nz = density.shape
+  n_total = nx * ny * nz
 
 
-for delta_max in delta_max_vals:
-  dens = density.copy()
+  for delta_max in delta_max_local:
+    dens = density.copy()
+    
+    file_name = output_dir + f'power_spectrum_{data_type}_{snap_id}_dens_max_{delta_max}.pkl'
+    if os.path.isfile( file_name ):
+      print( f'Skipping File: {file_name}' )
+      continue
+    
+    
+    dens_max = delta_max * rho_mean
+    indices = dens >= dens_max
+    fraction = indices.sum() / n_total
+    print( f"Delta max: {delta_max}   fraction: {fraction}") 
+    dens[dens>dens_max] = dens_max
 
-  dens_max = delta_max * rho_mean
-  indices = dens >= dens_max
-  fraction = indices.sum() / n_total
-  print( f"Delta max: {delta_max}   fraction: {fraction}") 
-  dens[dens>dens_max] = dens_max
+
+    print( f'Computing Power Spectrum  snap_id: {snap_id}  z:{z}' )
+    power_spectrum, k_vals, n_in_bin = get_power_spectrum( dens, Lbox, nx, ny, nz, dx, dy, dz,  n_kSamples=n_bins, fft_shift=False )
+    sim_data = { 'z':z, 'k_vals':k_vals, 'power_spectrum':power_spectrum, 'n_in_bin':n_in_bin }
 
 
-  print( f'Computing Power Spectrum  snap_id: {snap_id}  z:{z}' )
-  power_spectrum, k_vals, n_in_bin = get_power_spectrum( dens, Lbox, nx, ny, nz, dx, dy, dz,  n_kSamples=n_bins, fft_shift=False )
-  sim_data = { 'z':z, 'k_vals':k_vals, 'power_spectrum':power_spectrum, 'n_in_bin':n_in_bin }
-
-
-  file_name = output_dir + f'power_spectrum_{data_type}_{snap_id}_dens_max_{delta_max}.pkl'
-  Write_Pickle_Directory( sim_data, file_name )
+    Write_Pickle_Directory( sim_data, file_name )
 
