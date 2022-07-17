73072127b0309df2b04a569182b3f43921ba9e7d
bruno villasenor <bvillasen@gmail.com>
2022-07-17 17:45:33 -0500

    updated

--- initial_conditions/generate_ics_from_enzo_raw.py
+++ initial_conditions/generate_ics_from_enzo_raw.py
@@ -32,10 +32,10 @@ if 'particles' in types: ics_particles = True
 # n_points = 2048
 # n_boxes  = 1024
 
-# # Box Size
-# Lbox = 5000.0    #kpc/h
-# n_points = 1024
-# n_boxes  = 128
+# Box Size
+Lbox = 5000.0    #kpc/h
+n_points = 1024
+n_boxes  = 128
 
 # # Box Size
 # Lbox = 5000.0    #kpc/h
@@ -48,9 +48,9 @@ if 'particles' in types: ics_particles = True
 # n_boxes  = 64
 
 # # Box Size
-Lbox = 1000.0    #kpc/h
-n_points = 16
-n_boxes  = 1
+# Lbox = 1000.0    #kpc/h
+# n_points = 16
+# n_boxes  = 1
 
 L_Mpc = int( Lbox / 1000)
 
@@ -59,11 +59,11 @@ L_Mpc = int( Lbox / 1000)
 # input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_hydro_{L_Mpc}Mpc_cdm/raw/'
 # output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_{L_Mpc}Mpc_cdm/'
 
-m_wdm = 4.0
+m_wdm = 5.0
 # input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_dmo_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/raw/'
 # output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_dmo_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/'
-# input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_hydro_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/raw/'
-# output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/'
+input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_hydro_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/raw/'
+output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_{L_Mpc}Mpc_wdm_m{m_wdm:.1f}kev/'
 
 # input_dir = data_dir + f'cosmo_sims/ics/enzo/wdm/{n_points}_hydro_{L_Mpc}Mpc_cdm/raw/'
 # output_dir = data_dir + f'cosmo_sims/ics/wdm/{n_points}_{L_Mpc}Mpc_cdm/'
